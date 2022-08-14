5987cadf493e1f60c6598b47acc3bbf29cec6dd6
Azvya Erstevan I <erstevn@gmail.com>
2022-08-14 20:29:04 +0700

    Added margin on navbar when login

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -28,7 +28,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Tutup"></button>
             </div>
             <div class="offcanvas-body  align-items-center justify-content-between">
-                <ul class="navbar-nav  flex-grow-1 text-end pe-3">
+                <ul class="navbar-nav text-center flex-grow-1 text-end pe-lg-3">
                     <li class="nav-item  py-2 py-md-0">
                         <a class="nav-link px-3 <?= isSamePage('/') ?>" href="<?= base_url(); ?>">Beranda</a>
                     </li>
@@ -45,21 +45,21 @@
                     <?php endif ?>
                 </ul>
 
-                <ul class="navbar-nav pe-3 text-end align-items-lg-center">
+                <ul class="navbar-nav pe-lg-3 text-lg-end text-center align-items-lg-center">
 
                     <?php if (checkAuth('userId')) : ?>
                         <li class="nav-item py-2 py-md-0 px-3">
 
                             <span class="text-secondary"> Halo, <?= getUserName(); ?></span>
                         </li>
-                        <li class="nav-item dropdown py-2 py-md-0 px-3">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <li class="nav-item dropdown-center py-2 py-md-0 px-3">
+                            <a class="nav-link dropdown-toggle ms-3 ms-lg-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi text-secondary bi-person-circle" viewBox="0 0 16 16">
                                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                                 </svg>
                             </a>
-                            <ul class="dropdown-menu">
+                            <ul class="dropdown-menu text-center text-lg-start">
                                 <li>
                                     <a class="dropdown-item" href="<?= base_url('profil'); ?>">Profil</a>
                                 </li>
@@ -85,9 +85,9 @@
                             </ul>
                         </li>
                     <?php else : ?>
-                        <a href="<?= base_url('masuk'); ?>" class="btn btn-outline-secondary my-1 me-2 px-3">Masuk</a>
+                        <a href="<?= base_url('masuk'); ?>" class="btn btn-outline-secondary my-1 me-lg-2 px-3">Masuk</a>
                         <?php if ($callToAction = getCalltoAction()) : ?>
-                            <a target="<?= $callToAction->target; ?>" href="<?= $callToAction->cta_url; ?>" class="btn px-3 my-1 btn-primary me-2"><?= $callToAction->cta_text; ?></a>
+                            <a target="<?= $callToAction->target; ?>" href="<?= $callToAction->cta_url; ?>" class="btn px-3 my-1 btn-primary me-lg-2"><?= $callToAction->cta_text; ?></a>
                         <?php endif ?>
                     <?php endif ?>
 
