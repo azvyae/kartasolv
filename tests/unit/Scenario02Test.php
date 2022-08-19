b32cbd9360ed8c778e39ba4d24d55432b096d36f
Azvya Erstevan I <erstevn@gmail.com>
2022-08-19 22:27:04 +0700

    Finished test scenario from 00 to 02

--- tests/unit/Scenario02Test.php
+++ tests/unit/Scenario02Test.php
@@ -44,41 +44,78 @@ class Scenario02Test extends CIUnitTestCase
         $this->assertTrue($this->tc['expected'] === $this->tc['actual'], "expected: " . $this->tc['expected'] . "\n" . 'actual: ' . $this->tc['actual']);
     }
 
-    public function testForgetPasswordPage()
+    /**
+     * @testdox TC-01 Mengirim formulir lupa kata sandi tanpa data yang diinput
+     */
+    public function testForgetPasswordValidationFails()
     {
+        $this->tc['expected'] = "Menampilkan pesan Kolom Email harus diisi.";
         $result = $this->call('get', "lupa-kata-sandi");
         $result->assertOK();
         $result->assertSee('Lupa Kata Sandi', 'h1');
         $result->assertSeeElement('input[name=user_email]');
-    }
-
-    public function testForgetPasswordValidationFails()
-    {
+        $this->tc['step'] = [
+            'Masuk ke halaman lupa kata sandi',
+            'Tekan tombol lupa kata sandi',
+        ];
         $result = $this->call('post', "lupa-kata-sandi", [csrf_token() => csrf_hash(), 'g-recaptcha-response' => 'random-token']);
         $result->assertOK();
         $validationError = service('validation')->getError('user_email');
         $result->assertTrue($validationError === 'Kolom Email harus diisi.', $validationError);
         $result->assertRedirectTo(base_url('lupa-kata-sandi'));
+        $this->tc['actual'] = "Menampilkan pesan $validationError";
     }
 
+    /**
+     * @testdox TC-02 Mengirim formulir lupa kata sandi tanpa data yang diinput
+     */
     public function testForgetPasswordWithWrongEmail()
     {
-        $result = $this->call('post', "lupa-kata-sandi", ['user_email' => 'lala@gmail.com', csrf_token() => csrf_hash(), 'g-recaptcha-response' => 'random-token']);
+        $this->tc['expected'] = "Menampilkan pesan Email yang kamu tulis tidak ditemukan!";
+        $this->tc['step'] = [
+            'Masuk ke halaman lupa kata sandi',
+            'Isi kolom email',
+            'Tekan tombol lupa kata sandi',
+        ];
+        $this->tc['data'][] = 'user_email: notfound@gmail.com';
+        $result = $this->call('post', "lupa-kata-sandi", ['user_email' => 'notfound@gmail.com', csrf_token() => csrf_hash(), 'g-recaptcha-response' => 'random-token']);
         $result->assertOK();
         $result->assertSessionHas('message', 'Email yang kamu tulis tidak ditemukan!');
         $result->assertRedirectTo(base_url('lupa-kata-sandi'));
+        $this->tc['actual'] = 'Menampilkan pesan ' . getFlash('message', true);
     }
 
+    /**
+     * @testdox TC-03 Mengisi formulir lupa kata sandi dengan benar
+     */
     public function testForgetPasswordWithCorrectEmail()
     {
+        $this->tc['expected'] = "Menampilkan pesan Silakan cek emailmu untuk melanjutkan.";
+        $this->tc['step'] = [
+            'Masuk ke halaman lupa kata sandi',
+            'Isi kolom email',
+            'Tekan tombol lupa kata sandi',
+        ];
+        $this->tc['data'][] = 'user_email: test@test.com';
         $result = $this->call('post', "lupa-kata-sandi", ['user_email' => 'test@test.com', csrf_token() => csrf_hash(), 'g-recaptcha-response' => 'random-token']);
         $result->assertOK();
         $result->assertSessionHas('message', 'Silakan cek emailmu untuk melanjutkan.');
         $result->assertRedirectTo(base_url('masuk'));
+        $this->tc['actual'] = 'Menampilkan pesan ' . getFlash('message', true);
     }
 
+    /**
+     * @testdox TC-04 Meminta perubahan kata sandi berulang kali
+     */
     public function testForgetPasswordWithRecentlyRequestedAttempt()
     {
+        $this->tc['expected'] = "Menampilkan pesan Kamu baru saja melakukan permintaan atur ulang kata sandi, tunggu 5 menit lagi.";
+        $this->tc['step'] = [
+            'Masuk ke halaman lupa kata sandi',
+            'Isi kolom email',
+            'Tekan tombol lupa kata sandi',
+        ];
+        $this->tc['data'][] = 'user_email: test@test.com';
         $date = date('Y-m-d H:i:s', strtotime('+15 minutes', time()));
         $updateData = [
             'user_id' => 2,
@@ -89,27 +126,18 @@ class Scenario02Test extends CIUnitTestCase
         $result->assertOK();
         $result->assertSessionHas('message', 'Kamu baru saja melakukan permintaan atur ulang kata sandi, tunggu 5 menit lagi.');
         $result->assertRedirectTo(base_url('lupa-kata-sandi'));
+        $this->tc['actual'] = 'Menampilkan pesan ' . getFlash('message', true);
     }
 
-    public function testResetPasswordPage()
-    {
-        $date = date('Y-m-d H:i:s', strtotime('+15 minutes', time()));
-        $updateData = [
-            'user_id' => 2,
-            'user_reset_attempt' => $date
-        ];
-        $this->um->save($updateData);
-        $uuid = encode(2, 'resetPassword');
-        $attempt = encode(strtotime($date), 'resetPassword');
-        $result = $this->call('get', "atur-ulang-kata-sandi?uuid=$uuid&attempt=$attempt");
-        $result->assertOK();
-        $result->assertSee('Atur Ulang Kata Sandi', 'h1');
-        $result->assertSeeElement('input[name=user_new_password]');
-        $result->assertSeeElement('input[name=password_verify]');
-    }
-
+    /**
+     * @testdox TC-05 Masuk ke halaman atur ulang kata sandi, pengguna tidak ditemukan
+     */
     public function testResetPasswordUserNotFound()
     {
+        $this->tc['expected'] = "Menampilkan pesan Pengguna tidak ditemukan.";
+        $this->tc['step'] = [
+            'Masuk ke halaman atur ulang kata sandi',
+        ];
         $date = date('Y-m-d H:i:s', strtotime('+15 minutes', time()));
         $uuid = encode(999, 'resetPassword');
         $attempt = encode(strtotime($date), 'resetPassword');
@@ -117,10 +145,18 @@ class Scenario02Test extends CIUnitTestCase
         $result->assertOK();
         $result->assertSessionHas('message', 'Pengguna tidak ditemukan.');
         $result->assertRedirectTo(base_url('lupa-kata-sandi'));
+        $this->tc['actual'] = 'Menampilkan pesan ' . getFlash('message', true);
     }
 
+    /**
+     * @testdox TC-06 Masuk ke halaman atur ulang kata sandi, kadaluarsa
+     */
     public function testExpiredResetPassword()
     {
+        $this->tc['expected'] = "Menampilkan pesan Link tidak valid/kadaluarsa.";
+        $this->tc['step'] = [
+            'Masuk ke halaman atur ulang kata sandi',
+        ];
         $date = date('Y-m-d H:i:s', strtotime('-15 minutes', time()));
         $updateData = [
             'user_id' => 2,
@@ -133,10 +169,24 @@ class Scenario02Test extends CIUnitTestCase
         $result->assertOK();
         $result->assertSessionHas('message', 'Link tidak valid/kadaluarsa.');
         $result->assertRedirectTo(base_url('lupa-kata-sandi'));
+        $this->tc['actual'] = 'Menampilkan pesan ' . getFlash('message', true);
     }
 
+    /**
+     * @testdox TC-07 Gagal atur ulang kata sandi
+     */
     public function testResetPasswordValidationFails()
     {
+        $this->tc['expected'] = "Menampilkan pesan Kolom Kata Sandi Baru harus diisi.";
+        $this->tc['step'] = [
+            'Masuk ke halaman atur ulang kata sandi',
+            'Mengisi formulir perubahan kata sandi',
+            'Tekan tombol simpan',
+        ];
+        $this->tc['data'] = [
+            'user_new_password: (kosong)',
+            'password_verify: 123456'
+        ];
         $date = date('Y-m-d H:i:s', strtotime('+15 minutes', time()));
         $updateData = [
             'user_id' => 2,
@@ -155,10 +205,24 @@ class Scenario02Test extends CIUnitTestCase
         $validationError = service('validation')->getError('user_new_password');
         $result->assertTrue($validationError === 'Kolom Kata Sandi Baru harus diisi.', $validationError);
         $result->assertRedirectTo(base_url($url));
+        $this->tc['actual'] = "Menampilkan pesan $validationError";
     }
 
+    /**
+     * @testdox TC-08 Atur ulang kata sandi
+     */
     public function testResetPassword()
     {
+        $this->tc['expected'] = "Menampilkan pesan Berhasil mengubah kata sandi.";
+        $this->tc['step'] = [
+            'Masuk ke halaman atur ulang kata sandi',
+            'Mengisi formulir perubahan kata sandi',
+            'Tekan tombol simpan',
+        ];
+        $this->tc['data'] = [
+            'user_new_password: testpassword',
+            'password_verify: testpassword'
+        ];
         $date = date('Y-m-d H:i:s', strtotime('+15 minutes', time()));
         $updateData = [
             'user_id' => 2,
@@ -176,5 +240,6 @@ class Scenario02Test extends CIUnitTestCase
         $result->assertOK();
         $result->assertSessionHas('message', 'Berhasil mengubah kata sandi.');
         $result->assertRedirectTo(base_url('masuk'));
+        $this->tc['actual'] = "Menampilkan pesan " . getFlash('message');
     }
 }
