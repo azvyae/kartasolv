f9a6a96572c96337807e0a8df0fc5cd07c8370ee
Azvya Erstevan I <erstevn@gmail.com>
2023-02-16 00:02:47 +0700

    Added articles feature

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,19 +1,26 @@
-<?= $this->extend('layout/main_template'); ?>
-<?= $this->section('main'); ?>
+<?= $this->extend('layout/main_template') ?>
+<?= $this->section('main') ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
 <!-- HERO -->
 <div class="hero bg-warning text-dark" style="padding-top: 64px;">
-    <div class="container col-xxl-8 px-4 py-md-5 py-2  ">
+    <div class="col-xxl-8 py-md-5 container px-4 py-2">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="mx-auto col-10 col-sm-8 col-lg-6">
-                <img src="<?= base_url('img/logo.webp'); ?>" class="rounded mx-auto col-8 col-lg-8 d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="350" height="350" loading="lazy">
+            <div class="col-10 col-sm-8 col-lg-6 mx-auto">
+                <img src="<?= base_url('img/logo.webp') ?>"
+                    class="col-8 col-lg-8 d-block mx-lg-auto img-fluid mx-auto rounded" alt="Bootstrap Themes"
+                    width="350" height="350" loading="lazy">
             </div>
             <div class="col-lg-6 text-md-start text-center">
-                <h1 class="display-4 fw-bold lh-1 mb-3"><?= $landingInfo->landing_title; ?></h1>
-                <p class="lead"><?= $landingInfo->landing_tagline; ?></p>
-                <?php if ($callToAction = getCalltoAction()) : ?>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                        <a target="<?= $callToAction->target; ?>" type="button" href="<?= $callToAction->cta_url; ?>" class="btn btn-primary btn-lg px-4 me-md-2"><?= $callToAction->cta_text; ?></a>
+                <h1 class="display-4 fw-bold lh-1 mb-3">
+                    <?= $landingInfo->landing_title ?>
+                </h1>
+                <p class="lead">
+                    <?= $landingInfo->landing_tagline ?>
+                </p>
+                <?php if ($callToAction = getCalltoAction()): ?>
+                    <div class="d-grid d-md-flex justify-content-md-start gap-2">
+                        <a target="<?= $callToAction->target ?>" type="button" href="<?= $callToAction->cta_url ?>"
+                            class="btn btn-primary btn-lg me-md-2 px-4"><?= $callToAction->cta_text ?></a>
                     </div>
                 <?php endif ?>
             </div>
@@ -21,22 +28,28 @@
     </div>
 </div>
 <!-- VISI MISI -->
-<div class="bg-white text-dark">
-    <div class="container col-xxl-8 px-4 py-5  ">
+<div class="text-dark bg-white">
+    <div class="col-xxl-8 container px-4 py-5">
         <div class="row text-center">
             <h2 class="display-4 fw-bold">Visi & Misi</h2>
-            <p class="col-md-8 fs-6 mx-auto"><?= $landingInfo->vision; ?></p>
+            <p class="col-md-8 fs-6 mx-auto">
+                <?= $landingInfo->vision ?>
+            </p>
         </div>
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="col-10 mx-auto col-sm-8 col-lg-6">
-                <img src="<?= $landingInfo->landing_image; ?>" class="d-block mx-lg-auto img-fluid rounded" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+            <div class="col-10 col-sm-8 col-lg-6 mx-auto">
+                <img src="<?= $landingInfo->landing_image ?>" class="d-block mx-lg-auto img-fluid rounded"
+                    alt="Bootstrap Themes" width="700" height="500" loading="lazy">
             </div>
             <div class="col-lg-6 text-md-start text-center">
-                <?php foreach ($data = getMissions($landingInfo->mission) as $key => $m) : ?>
-                    <p class="fs-5"><?= $m['mission']; ?><br>
-                        <span class="fs-6"><?= $m['desc']; ?></span>
+                <?php foreach ($data = getMissions($landingInfo->mission) as $key => $m): ?>
+                    <p class="fs-5">
+                        <?= $m['mission'] ?><br>
+                        <span class="fs-6">
+                            <?= $m['desc'] ?>
+                        </span>
                     </p>
-                    <?php if (count($data) - 1 > $key) : ?>
+                    <?php if (count($data) - 1 > $key): ?>
                         <hr>
                     <?php endif ?>
                 <?php endforeach ?>
@@ -47,61 +60,120 @@
 </div>
 <!-- Kegiatan Kami -->
 <div class="bg-info bg-gradient text-primary text-center shadow-lg">
-    <div class="container col-xxl-8 px-4 py-5  ">
+    <div class="col-xxl-8 container px-4 py-5">
         <div class="row">
             <h2 class="display-4 fw-bold">Kegiatan Kami</h2>
-            <p class="col-md-8 fs-6 mx-auto">Ada serangkaian dan berbagai macam jenis kegiatan yang dilakukan oleh Karang Taruna Kelurahan Sarijadi, khususnya untuk menjalankan program kerja yang ada, yang memiliki berbagai macam jenis kegiatan.</p>
+            <p class="col-md-8 fs-6 mx-auto">Ada serangkaian dan berbagai macam jenis kegiatan yang dilakukan oleh
+                Karang Taruna Kelurahan Sarijadi, khususnya untuk menjalankan program kerja yang ada, yang memiliki
+                berbagai macam jenis kegiatan.</p>
         </div>
         <div class="row flex-lg-row align-items-center g-5 mt-md-2 py-3">
-            <div class="col-10 mx-auto col-sm-8 col-lg-6">
-                <img src="<?= $activitiesInfo->image_a; ?>" class="d-block mx-lg-auto shadow-lg   img-fluid rounded" alt="Bootstrap Themes" width="450" height="500" loading="lazy">
+            <div class="col-10 col-sm-8 col-lg-6 mx-auto">
+                <img src="<?= $activitiesInfo->image_a ?>" class="d-block mx-lg-auto img-fluid rounded shadow-lg"
+                    alt="Bootstrap Themes" width="450" height="500" loading="lazy">
             </div>
             <div class="col-lg-6">
-                <h3 class="fs-3 fw-bold col-lg-10 mx-auto"><?= $activitiesInfo->title_a; ?><br>
+                <h3 class="fs-3 fw-bold col-lg-10 mx-auto">
+                    <?= $activitiesInfo->title_a ?><br>
                 </h3>
-                <p class="fs-6 col-lg-8 mx-auto"><?= $activitiesInfo->desc_a; ?></p>
+                <p class="fs-6 col-lg-8 mx-auto">
+                    <?= $activitiesInfo->desc_a ?>
+                </p>
             </div>
         </div>
         <div class="row flex-lg-row-reverse align-items-center g-5 py-3">
-            <div class="col-10 mx-auto col-sm-8 col-lg-6">
-                <img src="<?= $activitiesInfo->image_b; ?>" class="d-block mx-lg-auto shadow-lg img-fluid rounded" alt="Bootstrap Themes" width="450" height="500" loading="lazy">
+            <div class="col-10 col-sm-8 col-lg-6 mx-auto">
+                <img src="<?= $activitiesInfo->image_b ?>" class="d-block mx-lg-auto img-fluid rounded shadow-lg"
+                    alt="Bootstrap Themes" width="450" height="500" loading="lazy">
             </div>
-            <div class="col-lg-6 ">
-                <h3 class="fs-3 fw-bold col-lg-10 mx-auto"><?= $activitiesInfo->title_b; ?><br>
+            <div class="col-lg-6">
+                <h3 class="fs-3 fw-bold col-lg-10 mx-auto">
+                    <?= $activitiesInfo->title_b ?><br>
                 </h3>
-                <p class="fs-6 col-lg-8 mx-auto"><?= $activitiesInfo->desc_b; ?></p>
+                <p class="fs-6 col-lg-8 mx-auto">
+                    <?= $activitiesInfo->desc_b ?>
+                </p>
             </div>
         </div>
         <div class="row flex-lg-row align-items-center g-5 py-3">
-            <div class="col-10 mx-auto col-sm-8 col-lg-6">
-                <img src="<?= $activitiesInfo->image_c; ?>" class="d-block mx-lg-auto shadow-lg img-fluid rounded" alt="Bootstrap Themes" width="450" height="500" loading="lazy">
+            <div class="col-10 col-sm-8 col-lg-6 mx-auto">
+                <img src="<?= $activitiesInfo->image_c ?>" class="d-block mx-lg-auto img-fluid rounded shadow-lg"
+                    alt="Bootstrap Themes" width="450" height="500" loading="lazy">
             </div>
-            <div class="col-lg-6 ">
-                <h3 class="fs-3 fw-bold col-lg-10 mx-auto"><?= $activitiesInfo->title_c; ?><br>
+            <div class="col-lg-6">
+                <h3 class="fs-3 fw-bold col-lg-10 mx-auto">
+                    <?= $activitiesInfo->title_c ?><br>
                 </h3>
-                <p class="fs-6 col-lg-8 mx-auto"><?= $activitiesInfo->desc_c; ?></p>
+                <p class="fs-6 col-lg-8 mx-auto">
+                    <?= $activitiesInfo->desc_c ?>
+                </p>
             </div>
         </div>
     </div>
 </div>
+<div class="bg-warning bg-gradient text-primary text-center shadow-lg">
+    <div class="col-xxl-10 container px-4 py-5">
+        <div class="row">
+            <h2 class="display-4 fw-bold">Berita</h2>
+            <p class="col-md-8 fs-6 mx-auto">Cek berita terbaru dari Kartasarijadi Press.</p>
+        </div>
+        <div class="row flex-lg-row align-items-center g-5 py-3">
+            <?php if (count($posts) > 0): ?>
+                <?php foreach ($posts as $post): ?>
+                    <div class="col-10 col-sm-8 col-lg-4 mx-auto">
+                        <a href="<?= $post->link ?>">
+                            <img style="object-fit: cover; width: 400px; height:250px" src="<?= $post->media ?>"
+                                class="d-block mx-lg-auto img-fluid rounded shadow-lg" alt="<?= $post->title ?>" loading="lazy">
+                        </a>
+                        <a href="<?= $post->link ?>" class="text-decoration-none">
+                            <h2 class="fs-4 fw-bold col-lg-10 mx-auto mt-4">
+                                <?= $post->title ?>
+                            </h2>
+                        </a>
+                        <p class="fs-6 mx-auto">
+                            <?= $post->excerpt ?>
+                        </p>
+                        <div class="d-flex justify-content-around">
+                            <span>
+                                <?= $post->date ?>
+                            </span>
+                            <a href="<?= $post->link ?>" class="small">
+                                Lihat Selengkapnya
+                            </a>
+                        </div>
+                    </div>
+                <?php endforeach ?>
+            <?php else: ?>
+                <span class="fs-3">Belum ada postingan</span>
+            <?php endif ?>
+        </div>
+    </div>
+</div>
 <!-- Siapa Kami -->
-<div class="bg-white text-dark">
-    <div class="container col-xxl-8 px-4 py-5  ">
+<div class="text-dark bg-white">
+    <div class="col-xxl-8 container px-4 py-5">
         <div class="row text-center">
             <h2 class="display-4 fw-bold">Siapa Kami</h2>
-            <p class="col-md-8 fs-6 mx-auto">Kami tidak sendiri, kami bergerak bersama, Warga Kelurahan Sarijadi dan para Pemudanya. Kami adalah Karang Taruna Ngajomantara.</p>
+            <p class="col-md-8 fs-6 mx-auto">Kami tidak sendiri, kami bergerak bersama, Warga Kelurahan Sarijadi dan
+                para Pemudanya. Kami adalah Karang Taruna Ngajomantara.</p>
         </div>
         <div class="row flex-lg-row align-items-center g-5 py-lg-5 py-2">
-            <div class="swiper ">
+            <div class="swiper">
                 <!-- Additional required wrapper -->
                 <div class="swiper-wrapper">
                     <!-- Slides -->
-                    <?php foreach ($members as $m) :  ?>
-                        <?php $urlFallback = "https://avatars.dicebear.com/api/initials/" . rawurlencode($m->member_name) . ".svg?background=%234F4F4F&fontSize=35&bold=true" ?>
+                    <?php foreach ($members as $m): ?>
+                        <?php $urlFallback = 'https://avatars.dicebear.com/api/initials/' . rawurlencode($m->member_name) . '.svg?background=%234F4F4F&fontSize=35&bold=true'; ?>
                         <div class="swiper-slide text-center">
-                            <img onerror="imageFallbackOption(this, '<?= $urlFallback; ?>')" src="<?= $m->member_image ?>" class="rounded-circle mb-4" width="150px" height="150px" style="object-fit: cover; object-position: top" alt="Foto <?= $m->member_name; ?>">
-                            <p class="fw-semibold col-lg-12 col-10 mx-auto mb-1 fs-5"><?= $m->member_name; ?></p>
-                            <p class="fs-6 col-lg-12 col-10 mx-auto"><?= $m->member_position; ?></p>
+                            <img onerror="imageFallbackOption(this, '<?= $urlFallback ?>')" src="<?= $m->member_image ?>"
+                                class="rounded-circle mb-4" width="150px" height="150px"
+                                style="object-fit: cover; object-position: top" alt="Foto <?= $m->member_name ?>">
+                            <p class="fw-semibold col-lg-12 col-10 fs-5 mx-auto mb-1">
+                                <?= $m->member_name ?>
+                            </p>
+                            <p class="fs-6 col-lg-12 col-10 mx-auto">
+                                <?= $m->member_position ?>
+                            </p>
                         </div>
                     <?php endforeach ?>
 
@@ -112,7 +184,7 @@
     </div>
 </div>
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         const navbar = document.querySelector('#main-navbar');
         hero = document.querySelector('.hero')
         height = Math.max(hero.scrollHeight, hero.offsetHeight,
@@ -135,5 +207,5 @@
     }
 </script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-<script src="<?= base_url('js/swiper.min.js'); ?>"></script>
-<?= $this->endSection(); ?>
+<script src="<?= base_url('js/swiper.min.js') ?>"></script>
+<?= $this->endSection() ?>
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,14 +1,19 @@
 <script src="https://www.google.com/recaptcha/api.js"></script>
-<nav id="main-navbar" class="navbar bg-gradient fw-semibold px-3 navbar-expand-lg <?= !isSameController(['Home']) ? 'bg-white shadow-sm' : '' ?> fixed-top">
+<nav id="main-navbar"
+    class="navbar bg-gradient fw-semibold px-3 navbar-expand-lg <?=!isSameController(['Home']) ? 'bg-white shadow-sm' : '' ?> fixed-top">
     <div class="container-fluid col-11">
         <div class="me-5">
-            <a href="<?= base_url(); ?>" class="navbar-brand  <?= !isSameController(['Home', 'Auth']) ? 'd-md-inline d-none' : '' ?>">
+            <a href="<?= base_url(); ?>"
+                class="navbar-brand  <?=!isSameController(['Home', 'Auth']) ? 'd-md-inline d-none' : '' ?>">
                 <img src="<?= base_url('img/logo.webp'); ?>" alt="Logo Karang Taruna" width="48" height="48">
             </a>
-            <?php if (!isSameController(['Home', 'Auth'])) : ?>
-                <button type="button" id="sidebarCollapse" class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#offCanvasSidebar" aria-controls="offCanvasSidebar">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-layout-sidebar-inset" viewBox="0 0 16 16">
-                        <path d="M14 2a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h12zM2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z" />
+            <?php if (!isSameController(['Home', 'Auth'])): ?>
+                <button type="button" id="sidebarCollapse" class="navbar-toggler" data-bs-toggle="offcanvas"
+                    data-bs-target="#offCanvasSidebar" aria-controls="offCanvasSidebar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                        class="bi bi-layout-sidebar-inset" viewBox="0 0 16 16">
+                        <path
+                            d="M14 2a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h12zM2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z" />
                         <path d="M3 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z" />
                     </svg>
                 </button>
@@ -16,10 +21,12 @@
         </div>
 
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="offcanvas offcanvas-end" style="width: 280px;" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas offcanvas-end" style="width: 280px;" tabindex="-1" id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel">
             <div class="offcanvas-header border-bottom">
                 <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
                     <img src="<?= base_url('img/logo.webp'); ?>" alt="Logo Karang Taruna" width="48" height="48">
@@ -33,30 +40,42 @@
                         <a class="nav-link px-3 <?= isSamePage('/') ?>" href="<?= base_url(); ?>">Beranda</a>
                     </li>
                     <li class="nav-item py-2 py-md-0">
-                        <a class="nav-link px-3 <?= isSamePage('sejarah') ?>" href="<?= base_url('sejarah'); ?>">Sejarah</a>
+                        <a class="nav-link px-3 <?= isSamePage('sejarah') ?>"
+                            href="<?= base_url('sejarah'); ?>">Sejarah</a>
                     </li>
                     <li class="nav-item py-2 py-md-0">
-                        <a class="nav-link px-3 <?= isSamePage('hubungi-kami') ?>" href="<?= base_url('hubungi-kami'); ?>">Hubungi Kami</a>
+                        <a class="nav-link px-3 <?= isSamePage('hubungi-kami') ?>"
+                            href="<?= base_url('hubungi-kami'); ?>">Hubungi Kami</a>
                     </li>
-                    <?php if (checkAuth('userId')) : ?>
+                    <?php if (checkAuth('userId')): ?>
                         <li class="nav-item py-2 py-md-0">
-                            <a class="nav-link px-3 <?= isSamePage('dasbor') ?>" href="<?= base_url('dasbor'); ?>">Dasbor</a>
+                            <a class="nav-link px-3 <?= isSamePage('dasbor') ?>"
+                                href="<?= base_url('dasbor'); ?>">Dasbor</a>
                         </li>
                     <?php endif ?>
+                    <li class="nav-item py-2 py-md-0">
+                        <a class="nav-link px-3 <?= isSamePage('artikel') ?>"
+                            href="<?= base_url('artikel'); ?>">Artikel</a>
+                    </li>
                 </ul>
 
                 <ul class="navbar-nav pe-lg-3 text-lg-end text-center align-items-lg-center">
 
-                    <?php if (checkAuth('userId')) : ?>
+                    <?php if (checkAuth('userId')): ?>
                         <li class="nav-item py-2 py-md-0 px-3">
 
-                            <span class="text-secondary"> Halo, <?= getUserName(); ?></span>
+                            <span class="text-secondary"> Halo,
+                                <?= getUserName(); ?>
+                            </span>
                         </li>
                         <li class="nav-item dropdown-center py-2 py-md-0 px-3">
-                            <a class="nav-link dropdown-toggle ms-3 ms-lg-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi text-secondary bi-person-circle" viewBox="0 0 16 16">
+                            <a class="nav-link dropdown-toggle ms-3 ms-lg-0" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                                    class="bi text-secondary bi-person-circle" viewBox="0 0 16 16">
                                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                                    <path fill-rule="evenodd"
+                                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                                 </svg>
                             </a>
                             <ul class="dropdown-menu text-center text-lg-start">
@@ -70,7 +89,9 @@
                                     <form id="logoutAttempt" action="<?= base_url('keluar'); ?>" method="post">
                                         <?= csrf_field(); ?>
                                         <input type="hidden" name="_method" value="DELETE">
-                                        <button type="button" data-sitekey="<?= getCaptchaSitekey(); ?>" data-callback='onLogout' data-action='logout' class="dropdown-item g-recaptcha">Keluar</button>
+                                        <button type="button" data-sitekey="<?= getCaptchaSitekey(); ?>"
+                                            data-callback='onLogout' data-action='logout'
+                                            class="dropdown-item g-recaptcha">Keluar</button>
 
                                     </form>
                                     <script>
@@ -84,10 +105,11 @@
                                 </li>
                             </ul>
                         </li>
-                    <?php else : ?>
+                    <?php else: ?>
                         <a href="<?= base_url('masuk'); ?>" class="btn btn-outline-secondary my-1 me-lg-2 px-3">Masuk</a>
-                        <?php if ($callToAction = getCalltoAction()) : ?>
-                            <a target="<?= $callToAction->target; ?>" href="<?= $callToAction->cta_url; ?>" id="callToAction" class="btn px-3 my-1 btn-primary me-lg-2"><?= $callToAction->cta_text; ?></a>
+                        <?php if ($callToAction = getCalltoAction()): ?>
+                            <a target="<?= $callToAction->target; ?>" href="<?= $callToAction->cta_url; ?>" id="callToAction"
+                                class="btn px-3 my-1 btn-primary me-lg-2"><?= $callToAction->cta_text; ?></a>
                         <?php endif ?>
                     <?php endif ?>
 
